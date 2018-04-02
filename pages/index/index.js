c145251b1da87cdd7570a33fd555d4e74dbbd0b4
PoBlue <250299430@qq.com>
2018-04-03 00:56:26 +0800

    [ui] viewpager & list

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,51 +5,40 @@ const network = require("../../utils/network.js")
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    isLoading: false,
+    newsList: [],
+    currentTab: 0,
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+    const self = this;
+
+    wx.getSystemInfo({
+      success: function (res) {
+        self.setData({
+          winWidth: res.windowWidth,
+          winHeight: res.windowHeight
+        });
       }
+    });
+
+    network.getNewsList('gn')
+      .then(res => {
+        this.setData({
+          newsList: res,
+        })
+      })
+  },
+  swichNav: function (e) {
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
     } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
+      this.setData({
+        currentTab: e.target.dataset.current
       })
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+  bindChange: function (e) {
+    var self = this;
+    self.setData({ currentTab: e.detail.current });
+  },
 })
